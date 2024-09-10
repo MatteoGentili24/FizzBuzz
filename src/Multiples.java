462decd1dd6b458a6c006c92f2c9419efa23b3b9
Matteo Gentili <mg@Matteos-MacBook-Pro-5.local>
2024-09-10 17:09:44 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,26 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        int count = 0;
+        while (i < 1000){
+
+            if (i % 3 == 0 ){
+                count += 1;
+            }
+
+            else if (i%5 == 0){
+                count += 1;
+            }
+
+            i +=1;
+        }
+
+        System.out.println(count);
+    }
+
+
+
+
+}
